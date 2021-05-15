79a1f0a00f4105b69503d069929a1ef44277b6fd
Ramon Lobo <ramonlobo633@gmail.com>
2021-05-14 23:36:13 -0300

    Adicionando solução do desafio 1470. Shuffle the Array

---
+++ ShuffleArray/SuffleArray.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        
+        vector <int> ax1;
+        vector <int> ax2;
+        vector <int> shuffle;
+        
+        if(){
+            
+        }
+        
+        for(int i = 1; i < n; i++){
+            
+        }
+        
+        
+        return suffle;
+    }
+};
+
+
